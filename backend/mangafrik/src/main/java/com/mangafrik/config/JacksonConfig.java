f74f4dee759746e54e8bdf38cb1319319555a535
Myahse <ahmanndesigning@gmail.com>
2026-04-23 15:45:02 +0000

    deployment backend

--- backend/mangafrik/src/main/java/com/mangafrik/config/JacksonConfig.java
+++ backend/mangafrik/src/main/java/com/mangafrik/config/JacksonConfig.java
@@ -3,10 +3,12 @@ package com.mangafrik.config;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class JacksonConfig {
 	@Bean
 	ObjectMapper objectMapper() {
