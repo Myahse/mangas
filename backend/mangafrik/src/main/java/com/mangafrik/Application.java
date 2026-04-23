252d5d2b1a18aec5eac284beda9c85b8d8d83715
Myahse <ahmanndesigning@gmail.com>
2026-04-23 17:04:20 +0000

    fix datasource

--- backend/mangafrik/src/main/java/com/mangafrik/Application.java
+++ backend/mangafrik/src/main/java/com/mangafrik/Application.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
+
+
 public class Application {
 
 	public static void main(String[] args) {
